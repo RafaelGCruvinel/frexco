243bdfaea1b5130ea9714a5b30e814bb7f6d80d9
Rafael Gonsalves Cruvinel <RCruvine@NEXTEL.COM.BR>
2019-05-13 15:16:31 -0300

    init

---
+++ precache-manifest.ac04d88b6ea99941c4c2bcba19e76f33.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47c5f4dfe86a134274d80a90b7934833",
+    "url": "/frexco/index.html"
+  },
+  {
+    "revision": "e044f15487697222f95a",
+    "url": "/frexco/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "c5dcd2ce0ed0437d0a7a",
+    "url": "/frexco/static/css/main.679ff28d.chunk.css"
+  },
+  {
+    "revision": "e044f15487697222f95a",
+    "url": "/frexco/static/js/2.9c190db6.chunk.js"
+  },
+  {
+    "revision": "c5dcd2ce0ed0437d0a7a",
+    "url": "/frexco/static/js/main.8973ef3c.chunk.js"
+  },
+  {
+    "revision": "23be56fb075548a6642a",
+    "url": "/frexco/static/js/runtime~main.fba2fe66.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/frexco/static/media/logo.5d5d9eef.svg"
+  }
+]);
